5bd0436765e755b82e29cbf48af0d9759f723cba
Skander Jabouzi <jabouzi@gmail.com>
2014-12-16 02:43:15 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/pizza/trunk@8 713efe2f-2740-4fd6-9d24-a04eb7563481

--- app/view/default/pizza/pizzaslist.php
+++ app/view/default/pizza/pizzaslist.php
@@ -1,9 +1,9 @@
 <div class="row"><?php display_message(); ?></div>
-<h1><?php echo lang('title.accounts'); ?></h1>
+<h1><?php echo lang('title.pizzas'); ?></h1>
 <div id="browsecontacts">
 <?php
-	foreach ($users as $user) {
-		echo '<a href="/'.get_site_lang().'/application/edit/' . $user->get_user_name() . '">';
+	foreach ($pizzas as $pizza) {
+		echo '<a href="/'.get_site_lang().'/pizza/edit/' . $user->get_pizza_id() . '">';
 		echo $user->get_user_first_name() . " " . $user->get_user_last_name();
 		echo '</a>';
 	}
