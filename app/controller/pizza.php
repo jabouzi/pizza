25572267abd94424381dcaf7704da9b0128e743e
Skander Jabouzi <jabouzi@gmail.com>
2014-12-16 03:12:53 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/pizza/trunk@12 713efe2f-2740-4fd6-9d24-a04eb7563481

--- app/controller/pizza.php
+++ app/controller/pizza.php
@@ -11,38 +11,14 @@ class Pizza extends Controller
 
 	public function index($message = null)
 	{
-		$pizzas = new pizzaiterator($this->pizzamodel->get_pizzas());
-		view::load_view('default/standard/header');
-		view::load_view('default/standard/menu');
-		if ($pizzas)
-		{
-			$data['pizzas'] = $pizzas;
-			view::load_view('default/pizza/pizzaslist', $data);
-		}
-		else
-		{
-			view::load_view('default/index/welcome');
-		}
-		view::load_view('default/standard/footer');
+		redirect('pizza/add');
 	}
 
 	public function add()
 	{
 		view::load_view('default/standard/header');
 		view::load_view('default/standard/menu');
-		view::load_view('default/accounts/add');
-		view::load_view('default/standard/footer');
-		unset($_SESSION['request']);
-	}
-
-	public function edit($pizza_name)
-	{
-		$pizza = $this->pizzamodel->get_pizza($pizza_name);
-		$data['pizza'] = $pizza;
-		$_SESSION['pizza_edit'] = $pizza->__toArray();
-		view::load_view('default/standard/header');
-		view::load_view('default/standard/menu');
-		view::load_view('default/pizza/edit', $data);
+		view::load_view('default/pizza/add');
 		view::load_view('default/standard/footer');
 		unset($_SESSION['request']);
 	}
@@ -53,13 +29,13 @@ class Pizza extends Controller
 		{
 			$_SESSION['request'] = $_POST;
 			$_SESSION['message'] = lang('account.email.exists');
-			redirect('application/add');
+			redirect('pizza/add');
 		}
 		else if ($this->pizzamodel->pizza_name_exists($_POST['pizza_name']))
 		{
 			$_SESSION['request'] = $_POST;
 			$_SESSION['message'] = lang('account.pizza.name.exists');
-			redirect('application/add');
+			redirect('pizza/add');
 		}
 		else
 		{
@@ -67,32 +43,6 @@ class Pizza extends Controller
 			$this->sendemail($_POST, self::ADD);
 			$_SESSION['message'] = lang('account.pizza.added');
 			redirect('/');
-		}
-	}
-
-	public function processedit()
-	{
-		if ($_SESSION['pizza_edit']['pizza_name'] != $_POST['pizza_name'])
-		{
-			$_SESSION['message'] = lang('account.security.detected');
-			redirect('application/edit/'.$_SESSION['pizza_edit']['pizza_name']);
-		}
-		else if ($this->pizzamodel->pizza_email_exists($_POST['pizza_email'], $_POST['pizza_name']))
-		{
-			$_SESSION['request'] = $_POST;
-			$_SESSION['message'] = lang('account.email.exists');
-			redirect('application/edit/'.$_POST['pizza_name']);
-		}
-		else
-		{
-			if (count(compare_pizza_data($_POST, $_SESSION['pizza_edit'])))
-			{
-				$this->pizzamodel->update_pizza($_POST);
-				$pizza = $this->pizzamodel->get_pizza($_POST['pizza_name'])->__toArray();
-				$this->sendemail($pizza, self::EDIT);
-				$_SESSION['message'] = lang('account.pizza.updated');
-			}
-			redirect('application/edit/'.$_POST['pizza_name']);
 		}
 	}
 }
--- app/view/default/pizza/add.php
+++ app/view/default/pizza/add.php
@@ -1,36 +1,57 @@
 <div class="row"><?php display_message(); ?></div>
-<h2><?php echo lang('title.add.account'); ?></h2>
-<form action="/<?php echo get_site_lang(); ?>/application/processadd" method="post" id="addform" name="addform">
-	<div class="row">
-        <input type="hidden" name="user_group" id="user_group" value=""/> 
-    </div>
+<h2><?php echo lang('title.add.pizza'); ?></h2>
+<form action="/<?php echo get_site_lang(); ?>/pizza/processadd" method="post" id="addform" name="addform">
     <div class="row">
-		<label for="user_name"><?php echo lang('form.username'); ?>:</label>
-		<input type="text" name="user_name" id="user_name" value="<?php echo print_post_text('user_name'); ?>" data-validate="required" />
-	</div>
-	<div class="row">
-		<label for="user_password"><?php echo lang('form.password'); ?>:</label>
-		<input type="password" name="user_password" id="user_password" value="" data-validate="required" autocomplete="off" />
-	</div>
-    <div class="row">
-		<label for="email"><?php echo lang('form.email'); ?>:</label>
-		<input type="text" name="user_email" id="user_email" value="<?php echo print_post_text('user_email'); ?>" data-validate="required" />
+		<label for="phone"><?php echo lang('form.phone'); ?>:</label>
+		<input type="text" name="phone" id="phone" value="<?php echo print_post_text('phone'); ?>" data-validate="required" />
 	</div>
     <div class="row">
 		<label for="first_name"><?php echo lang('form.firstname'); ?>:</label>
-		<input type="text" name="user_first_name" id="user_first_name" value="<?php echo print_post_text('user_first_name'); ?>" data-validate="required"  />
+		<input type="text" name="first_name" id="first_name" value="<?php echo print_post_text('first_name'); ?>" data-validate="required"  />
 	</div>
 	<div class="row">
 		<label for="last_name"><?php echo lang('form.lastname'); ?>:</label>
-		<input type="text" name="user_last_name" id="user_last_name" value="<?php echo print_post_text('user_last_name'); ?>" data-validate="required" />
+		<input type="text" name="last_name" id="last_name" value="<?php echo print_post_text('last_name'); ?>" data-validate="required" />
 	</div>
+    <div class="row">
+		<label for="address"><?php echo lang('form.address'); ?>:</label>
+		<input type="text" name="address" id="address" value="<?php echo print_post_text('address'); ?>" data-validate="required" />
+	</div>
+    <div class="row">
+		<label for="address2"><?php echo lang('form.address2'); ?>:</label>
+		<input type="text" name="address2" id="address2" value="<?php echo print_post_text('address2'); ?>" data-validate="required" />
+	</div>
+    <div class="row">
+		<label for="comments"><?php echo lang('form.comments'); ?>:</label>
+		<textarea name="comments" id="comments" maxlength="100" data-validate="required"><?php echo print_post_text('comments'); ?></textarea>
+	</div>
+	<hr>
 	<div class="row">
-		<label><?php echo lang('form.hosts'); ?>:</label>
-		<?php get_projects(); ?>
+		<label for="ingredient_1"><?php echo lang('form.ingredient1'); ?>:</label>
+		<input type="checkbox" name="ingredient_1" id="ingredient_1" value="1" <?php if (intval(print_post_text('ingredient_1')) == 1) echo 'checked'; ?>>
+    </div>
+	<div class="row">
+		<label for="ingredient_2"><?php echo lang('form.ingredient2'); ?>:</label>
+		<input type="checkbox" name="ingredient_2" id="ingredient_2" value="1" <?php if (intval(print_post_text('ingredient_2')) == 1) echo 'checked'; ?>>
+    </div>
+	<div class="row">
+		<label for="ingredient_3"><?php echo lang('form.ingredient1'); ?>:</label>
+		<input type="checkbox" name="ingredient_3" id="ingredient_3" value="1" <?php if (intval(print_post_text('ingredient_3')) == 1) echo 'checked'; ?>>
+    </div>
+	<div class="row">
+		<label for="delivery"><?php echo lang('form.type'); ?>:</label>
+		<select name="delivery" id="delivery">
+			<option value="1">Delivery</option>
+			<option value="0">Pickup</option>
+		</select>
+    </div>
+    <div class="row">
+		<label for="price"><?php echo lang('form.price'); ?>:</label>
+		<input type="text" name="price" id="price" value="<?php echo print_post_text('price'); ?>" data-validate="required" />
 	</div>
     <div>
 		<div class="row"><label for="submit"> </label>
-			<input id="submitadd" type="button" value="<?php echo lang('title.add.account'); ?>" class="submitbutton"/>
+			<input id="submitadd" type="button" value="<?php echo lang('title.add.pizza'); ?>" class="submitbutton"/>
 		</div>
 		<div class="row error_form_msg">
 			<span class="error"><?php echo lang('form.check.required.fields'); ?></span>
